6d93ec684f18a12d7f42b0c258f619f22663dec7
f85084 <f85084@yahoo.com.tw>
2015-06-05 19:45:08 +0800

    微更新
    微更新

--- adduser.php
+++ adduser.php
@@ -13,9 +13,10 @@ include ('mydb.php');
 //上傳
 $upload_dir='./upload/';
 //上傳檔案 並改名稱
-if(@$_FILES['UpFile']['error']==UPLOAD_ERR_OK){
-move_uploaded_file($_FILES['UpFile']['tmp_name'],
-                   $upload_dir . $_FILES['UpFile']['name']);
+if(@$_FILES['file']['error']==UPLOAD_ERR_OK){
+	//把存檔的檔案搬到上傳目錄下 並改回原始檔名
+move_uploaded_file($_FILES['file']['tmp_name'],
+                   $upload_dir . $_FILES['file']['name']);
 echo '上傳成功';
 }
 else{
@@ -27,11 +28,12 @@ echo "上傳失敗";
 	$name=$_POST['name'];
 	$tel=$_POST['tel'];
 	$address=$_POST['address'];
-	$file=$_POST['file'];
+	$file=$_FILES['file']['name'];
 	$memberdate=$_POST['memberdate'];
     $sql="INSERT member (id,password,name,tel,address,file,memberdate)
-        VALUES ('{$id}','{$password}','{$name}','{$tel}','{$address}','{$_FILES['UpFile']['name']}','{$memberdate}')";
-	$result=mysql_query($sql);
+        VALUES ('{$id}','{$password}','{$name}','{$tel}','{$address}','$file',sysdate())";
+		echo $sql;
+	//$result=mysql_query($sql);
 	//異動會顯示異動資料
 	if (mysql_affected_rows()>=1);
 	echo '新增成功<br><br>';  
