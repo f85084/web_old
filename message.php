b380a3d2c0c258652e5d812b3e60f0dd3a2a9fda
f85084 <f85084@yahoo.com.tw>
2015-06-10 17:58:10 +0800

    把CSS拿掉
    把CSS拿掉

--- message.php
+++ message.php
@@ -16,45 +16,20 @@ if (!mysql_fetch_array($result))
 <title>留言</title>
 <meta name=viewport content="width=device-width, initial-scale=1.0">
 <meta name=description content=""><meta name=author content="">
-<link href=http://kkbruce.tw/Content/BS2/bootstrap.css  rel=stylesheet>
+<link href=http://f85084.github.io/css.css  rel=stylesheet>
 <style>
 /*整體字型、背景*/
 body{
 	font-family: Arial, 微軟正黑體;
 	background-color:#f5f5f5
 	}
-.form-Search{
-max-width:300px;
-padding:19px 29px 29px;
-margin:0 auto 20px;
-background-color:#fff;border:1px solid #e5e5e5;
-font-size:110%;
--webkit-border-radius:5px;
--moz-border-radius:5px;
-border-radius:5px;
--webkit-box-shadow:0 1px 2px rgba(0,0,0,.05);
--moz-box-shadow:0 1px 2px rgba(0,0,0,.05);
-box-shadow:0 1px 2px rgba(0,0,0,.05)}
-.form-signin {
-width:500pX;
-/*區塊置中*/
-margin: 0px auto;;
-}
-.checkbox{margin-bottom:20px}
-.form-signin input[type="text"],
-.form-signin input[type="password"]
-{
-font-size:16px;height:auto;
-margin-bottom:15px;padding:7px 9px
-}
+
 </style>
-<link href=/Content/BS2/bootstrap-responsive.css rel=stylesheet> 
+<!--<link href=/Content/BS2/bootstrap-responsive.css rel=stylesheet> -->
 <!--[if lt IE 9]><script src=~/Scripts/BS2/html5shiv.js></script><![endif]-->
 <body>
 <body><br>
-<?
-echo '你好'.$row['name'];
-?>
+
 <div>
 <form name="login" method="post" action="addmessage.php" class=form-signin>
 <h2 class=form-signin-heading>留言</h2>
