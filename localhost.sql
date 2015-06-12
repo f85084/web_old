1139ecb47c7d899ca2148e62bc9452212d103005
f85084 <f85084@yahoo.com.tw>
2015-06-12 10:29:54 +0800

    SQL
    SQL

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- 主機: localhost
--- 建立日期: Jun 03, 2015, 10:06 AM
+-- 建立日期: Jun 12, 2015, 02:29 AM
 -- 伺服器版本: 5.0.45
 -- PHP 版本: 5.2.3
 
@@ -28,12 +28,35 @@ CREATE TABLE `member` (
   `name` varchar(20) NOT NULL,
   `tel` varchar(20) NOT NULL,
   `address` varchar(20) NOT NULL,
-  `file` varchar(20) NOT NULL,
-  `member-date` date NOT NULL,
+  `gif` varchar(20) NOT NULL,
+  `memberdate` datetime NOT NULL,
   PRIMARY KEY  (`number`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
 
 -- 
 -- 列出以下資料庫的數據： `member`
+-- 
+
+INSERT INTO `member` VALUES (25, 'a123', '123123', '國際tw', '0977777777', '台北', '200x100_6.jpg', '2015-06-10 16:59:41');
+INSERT INTO `member` VALUES (24, 'a1123', '1232', '國際tw', '00998', '台北', '200x100_2.jpg', '2015-06-10 14:19:03');
+INSERT INTO `member` VALUES (23, '', '', '', '', '', '', '2015-06-10 14:18:20');
+
+-- --------------------------------------------------------
+
+-- 
+-- 資料表格式： `message`
+-- 
+
+CREATE TABLE `message` (
+  `no` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `Content` varchar(200) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY  (`no`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 
+-- 列出以下資料庫的數據： `message`
 -- 
 
