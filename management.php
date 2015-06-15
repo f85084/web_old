ca4b7fa3c04a1106501a4313fe6186b444857714
f85084 <zz147852>
2015-06-15 19:54:44 +0800

    微調整
    微調整

--- management.php
+++ management.php
@@ -1,37 +1,18 @@
 <!DOCTYPE html>
 <html lang=en><head><meta charset=utf-8>
-<title>帳號表單</title>
+<title>帳號</title>
 <meta name=viewport content="width=device-width, initial-scale=1.0">
 <meta name=description content=""><meta name=author content="">
-<link href=http://kkbruce.tw/Content/BS2/bootstrap.css  rel=stylesheet>
+<link href=http://f85084.github.io/css.css  rel=stylesheet>
 <style>
+/*整體字型、背景*/
 body{
-	padding:19px 290px 29px;
 	font-family: Arial, 微軟正黑體;
 	background-color:#f5f5f5
 	}
-.form-Search{
-max-width:300px;
-padding:19px 29px 29px;
-margin:0 auto 20px;
-background-color:#fff;border:1px solid #e5e5e5;
-font-size:110%;
--webkit-border-radius:5px;
--moz-border-radius:5px;
-border-radius:5px;
--webkit-box-shadow:0 1px 2px rgba(0,0,0,.05);
--moz-box-shadow:0 1px 2px rgba(0,0,0,.05);
-box-shadow:0 1px 2px rgba(0,0,0,.05)}
-.form-signin 
-.checkbox{margin-bottom:10px}
-.form-signin input[type="text"],
-.form-signin input[type="password"]
-{
-font-size:16px;height:auto;
-margin-bottom:15px;padding:7px 9px
-}
+
 </style>
-<link href=/Content/BS2/bootstrap-responsive.css rel=stylesheet> 
+<!--<link href=/Content/BS2/bootstrap-responsive.css rel=stylesheet> -->
 <!--[if lt IE 9]><script src=~/Scripts/BS2/html5shiv.js></script><![endif]-->
 <body>
   <!--查詢-->
