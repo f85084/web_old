354168b41ad8246fde00128ff8e47548f8429742
f85084 <f85084@yahoo.com.tw>
2015-07-23 18:04:12 +0800

    沒輸入東西無法登入
    沒輸入東西無法登入

--- Log in.php
+++ Log in.php
@@ -19,17 +19,17 @@ session_start();
 $_SESSION['flag']='0';
 
 ?>
-<form name="login" method="get" action="message.php" class=form-signin onclick="return false">
+<form name="login" method="get" action="message.php" class=form-signin id="login" >
 <h2 class=form-signin-heading>請登入</h2>
 <p>
-<input type="text" name="id" class="input-block-level" placeholder="輸入帳號 "> 
-<input type="password" name="password" class="input-block-level" placeholder="輸入密碼">
+<input type="text" id="id" name="id" class="input-block-level" placeholder="輸入帳號 "> 
+<input type="password" id="password" name="password" class="input-block-level" placeholder="輸入密碼">
   <label class=checkbox> 
     </br>
   </label>
 </p>
 <label class=checkbox>
-  <button class="btn btn-large btn-primary" type=submit>登入</button>
+  <button class="btn btn-large btn-primary" id="submitBtn" type=submit onclick="return false">登入</button>
   </br>
   <input type=checkbox value=remember-me> 忘記密碼 </label>
 </form>
@@ -37,3 +37,29 @@ $_SESSION['flag']='0';
 <div class="h8">
   <a href="newuser.php"><button class="btn btn-large btn-primary" type=submit>註冊</button> </a>
   </div> 
+<script>
+// 取得送出按鈕Tag
+var btn = document.getElementById("submitBtn");
+// 傾聽點擊事件
+btn.addEventListener("click", function(){
+	// 取得帳號輸入值
+	var id = document.getElementById("id").value;
+	// 取得密碼輸入值
+	var password = document.getElementById("password").value;
+	// 如果帳號沒有輸入, 顯示帳號未輸入
+	if(!id) {
+		alert('請輸入帳號');
+		return false;
+	}
+	// 如果密碼沒有輸入, 顯示密碼未輸入
+	if(!password) {
+		alert('請輸入密碼');
+		return false;
+	}
+	// 取得表單, 送出
+	var login = document.getElementById("login");
+	login.submit();
+});
+</script>
+</body>
+</html>
