510b47bc242e7322aa5f9d455fdadf0ce8ef143e
f85084 <f85084@yahoo.com.tw>
2015-11-19 17:58:40 +0800

    更換首頁版型
    更換首頁版型

--- index.php
+++ index.php
@@ -1,78 +1,106 @@
 <!DOCTYPE html>
 <html lang=en>
-
 <head>
     <meta charset=utf-8>
-    <meta http-equiv=X-UA-Compatible content="IE=edge">
-    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>An's旅</title>
+    <meta name=viewport content="width=device-width, initial-scale=1.0">
     <meta name=description content="">
     <meta name=author content="">
-    <link rel=icon href=/Content/AssetsBS3/img/favicon.ico>
-    <title>An's旅</title>
-    <link href=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css rel=stylesheet>
+    <link href=/Content/BS2/bootstrap.css rel=stylesheet>
+    <style>
+        body {
+            padding-top: 60px;
+            padding-bottom: 40px;
+        }
+    </style>
+    <link href=css/index/index1119.css rel=stylesheet> <!--[if lt IE 9]><script src=~/Scripts/BS2/html5shiv.js></script><![endif]-->
+    <link href=css/index/bootstrap-responsive.css rel=stylesheet>
+    <link rel=apple-touch-icon-precomposed sizes=144x144 href=/Images/BS2/ico/apple-touch-icon-144-precomposed.png>
+    <link rel=apple-touch-icon-precomposed sizes=114x114 href=/Images/BS2/ico/apple-touch-icon-114-precomposed.png>
+    <link rel=apple-touch-icon-precomposed sizes=72x72 href=/Images/BS2/ico/apple-touch-icon-72-precomposed.png>
+    <link rel=apple-touch-icon-precomposed href=/Images/BS2/ico/apple-touch-icon-57-precomposed.png>
+    <link rel="shortcut icon" href=/Images/BS2/ico/favicon.png>
+
     <link href=css/index/css1026.css rel=stylesheet type="text/css">
-    <!--[if lt IE 9]><script src=~/Scripts/AssetsBS3/ie8-responsive-file-warning.js></script><![endif]-->
-    <script src=/Scripts/AssetsBS3/ie-emulation-modes-warning.js></script>
-    <!--[if lt IE 9]><script src=https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js></script><script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script><![endif]-->
-    </head>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top" role=navigation>
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class=navbar-inner>
             <div class=container>
-                <div class=navbar-header>
-                    <button type=button class="navbar-toggle collapsed" data-toggle=collapse data-target=#navbar aria-expanded=false aria-controls=navbar> <span class=sr-only>Toggle navigation</span> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </button> 
-                    <a class=navbar-brand href=index.php>An's旅</a></div>
-                <div id=navbar class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class=active><a href=index-share.php>分享</a>
-                             <li class=dropdown><a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-expanded=false>國外<span class=caret></span></a>
-                                        <ul class=dropdown-menu role=menu>
-                                          <li><a href=#>日本</a>
-                                           <li><a href=#>韓國</a>
-                                                </ul>
-                               <li class=dropdown><a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-expanded=false>國內 <span class=caret></span></a>
-                                        <ul class=dropdown-menu role=menu>
-                                          <li><a href=#>北</a>
-                                          <li><a href=#>中</a>
-                                          <li><a href=#>南</a>
-                               </ul>
-                                    <li class=dropdown><a href=# class=dropdown-toggle data-toggle=dropdown role=button aria-expanded=false>關於 <span class=caret></span></a>
-                                        <ul class=dropdown-menu role=menu>
-                                          <li><a href=#>公告</a>
-                                          <li><a href=#>簡介</a>
-                                          <li><a href=#>連結</a>
-                                             <li class=divider>
-                                              <li class=dropdown-header>會員
-                                                                <li><a href=#>登入</a>
-                                                                 </ul>
+                <button type=button class="btn btn-navbar" data-toggle=collapse data-target=.nav-collapse> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </button>
+                <a class=brand href=index.php>An's旅</a>
+                <div class="nav-collapse collapse">
+                  <ul class=nav>
+                  <li class=dropdown>                  
+                  <li class=dropdown><a href=index-share.php>分享</a>
+                  <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown>國外<b class=caret></b></a>
+                    <ul class=dropdown-menu>
+                      <li><a href=#>日本</a>
+                      <li><a href=#>韓國</a>
                     </ul>
-                </div>
+                  <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown>國內<b class=caret></b></a>
+                    <ul class=dropdown-menu>
+                      <li><a href=#>北</a>
+                      <li><a href=#>中</a>
+                      <li><a href=#>南</a>
+                    </ul>
+                  <li class=dropdown> <a href=# class=dropdown-toggle data-toggle=dropdown>Dropdown <b class=caret></b></a>
+                    <ul class=dropdown-menu>
+                      <li><a href=#>公告</a>
+                      <li><a href=#>簡介</a>
+                      <li><a href=#>連結</a>
+                      <li class=divider>                    
+                      <li class=nav-header>Nav header
+                      <li><a href=#>Separated link</a>
+                      <li><a href=#>One more separated link</a>
+                    </ul>
+                                    </ul>
+                  <form class="navbar-form pull-right">
+                  <input class=span2 placeholder=Email> <input class=span2 type=password placeholder=Password> 
+                  <button type=submit class=btn>Sign in</button></form>
+</div>
             </div>
-        </nav>
-        <div class=container>
-            <div class=page-header>
-                <h1>Sticky footer with fixed navbar</h1></div>
-            <p class=lead>Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.
-                <p>Back to <a href="/bs3/Examples?name=sticky-footer">the default sticky footer</a> minus the navbar.</div>
-                             <div class=container>
-                             <div class=jumbotron>
-                            <h1>Hello, world!</h1>
-                            <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.<img id="fx" src="photo/index/2015-10-1.jpg"/>
-</div></div>
-
-        <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js></script>
-        <script src=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js></script>
-        <script src=/Scripts/AssetsBS3/ie10-viewport-bug-workaround.js></script>
-        <!--FOOTER S-->
-    <footer class="rwd-footer clearfix">
-
-        <ul class="contact">
-            <li><a href="tel:0800"><i class="icon-phone"></i>0800</a></li>
-            <li><a href="mailto:Anna's"><i class="icon-email"></i>Anna's</a></li>
-        </ul>
+        </div>
     </div>
-    <p class="copyright">© <span id="spanCopyYear">1990</span> Anna's All Rights Reserved.</p>
+    <div class=container>
+        <div class=hero-unit>
+            <h1>Hello, world!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+            <p><a href=# class="btn btn-primary btn-large">Learn more &raquo;</a>
+        </div>
+        <div class=row>
+            <div class=span4>
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                <p><a class=btn href=#>View details &raquo;</a>
+            </div>
+            <div class=span4>
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                <p><a class=btn href=#>View details &raquo;</a>
+            </div>
+            <div class=span4>
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                <p><a class=btn href=#>View details &raquo;</a>
+            </div>
+        </div>
+</div>
+<hr>
+<!--FOOTER S-->
+<footer class="rwd-footer clearfix">
+
+  <ul class="contact">
+    <li><a href="tel:0800"><i class="icon-phone"></i>0800</a></li>
+    <li><a href="mailto:Anna's"><i class="icon-email"></i>Anna's</a></li>
+  </ul>
+        </div>
+        <p class="copyright">© <span id="spanCopyYear">1990</span> Anna's All Rights Reserved.</p>
 </footer>
-    <!--FOOTER E-->
-	<script type="text/javascript">
-	    document.getElementById('spanCopyYear').innerHTML = (new Date()).getFullYear();</script>    
-        </body>
+<!--FOOTER E-->
+<script type="text/javascript">
+        document.getElementById('spanCopyYear').innerHTML = (new Date()).getFullYear();
+    </script>
+
+<script src="css/index/index.js"></script>
+</body>
