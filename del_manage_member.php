ec8688d4f631854309a573de88cb3f7363ce0f73
Anna <f85084@yahoo.com.tw>
2016-06-03 18:08:30 +0800

    新增刪除人員
    刪除人員頁面尚未完成

--- del_manage_member.php
+++ del_manage_member.php
@@ -1,50 +1,22 @@
 <?
 include ('mydb.php');
-include ('index_action.php');
+//include ('index_action.php');
 session_start();
-$_SESSION['flag']='1';
-$id=$_GET['id'];
-$wherea=array();
-$where='';
-$logws='';
-$lurl='';
-if(!empty($_GET['key_id'])){
-	$wherea[]="id like '%".$_GET['key_id']."%'";
-	$logw[]="id like ".$_GET['key_id'];
-}
-if(!empty($wherea)){
-	$where=" WHERE ".implode(' and ',$wherea);
-	$logws=implode(' and ',$logw);
-}
-$number=10;
-/*總共幾筆*/
-$query_num= "SELECT COUNT(*) FROM member ";	
-$num=mysql_query($query_num);
-$q_num =mysql_fetch_row($num);
-$product_page_num= $q_num[0];
-$page=ceil($product_page_num/$number);
- /*頁設設定*/
-if(isset($_GET['p'])){
-	$p=$_GET['p'];
-	$start=($p-1)*$number;
-}
-else{
-	$start=0;
-}
-$querymr = "SELECT * FROM  member $where order by  memberdate  ASC LIMIT $start, $number";
-$mr=mysql_query($querymr);
-$admin_group=array(1=>'訪客',2=>'會員',3=>'操作人員',4=>'管理者')	;
-
+$_SESSION['number']=$row['number'];
+$_SESSION['number']=$_GET['number'];
+$sql="select * from member where number=$_SESSION[number]";
+$result=mysql_query($sql);
+$row=mysql_fetch_array($result);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-Hant">
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
+        <link rel=icon href=photo/index/an_logo.ico>
     <title>後台</title>
     <!-- Bootstrap Core CSS -->
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -58,10 +30,17 @@ $admin_group=array(1=>'訪客',2=>'會員',3=>'操作人員',4=>'管理者')	;
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
     <!-- Custom Fonts -->
     <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<style>
+/*整體字型、背景*/
+body{
+	font-family: Arial, 微軟正黑體;
+	}
+</style>
 </head>
 <body>
     <!--目錄-->
     <div id="wrapper">
+
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
@@ -333,78 +312,61 @@ $admin_group=array(1=>'訪客',2=>'會員',3=>'操作人員',4=>'管理者')	;
             <!-- /.navbar-static-side -->
         </nav>
         <!--目錄-->
-        <!--內容B-->
+                <!--內容B-->
         <div id="page-wrapper">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+ <form name="del_manage_member.php" method="post" action="del_manage_member_action.php"  enctype="multipart/form-data" class=form-signin>
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">刪除基本資料</div>
+  <div class="panel-body">
+    <p>...</p>
+  </div>
+  <!-- List group -->
+  <ul class="list-group">
+    <li class="list-group-item"><tr width="150" align="center"><td >登入密碼</td><td><?= $row[password]; ?></td></tr></li>
+    <li class="list-group-item"></li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div>             
+					<div >
+					<tr>
+					<td>
+						<label></label>
+						
+					</td>
+						<label>姓名</label>
+						<input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="姓名" value="<? echo $row[name]; ?>"> 
+					</tr>
+					</div>
+					<div class="form-group">
+						<label>電話</label>
+						<input type="text" name="tel" class="form-control" id="exampleInputEmail1"  placeholder="電話" value="<? echo $row[tel]; ?>"> 
+					</div>
+					<div class="form-group">
+						<label>地址</label>
+						<input type="text" name="address" class="form-control" id="exampleInputEmail1" placeholder="地址" value="<? echo $row[address]; ?>"> 
+					</div>                        
+					<div class="form-group">
+						<label>上傳照片</label>
+						<input type="file" name="gif" id="exampleInputFile" placeholder="上傳照片" value="<? echo $row[gif]; ?>"> 
+						<p class="help-block">會員圖片</p>
+					</div>
+				   <a href="edit_manage_member_action.php">
+					<button type="submit" class="btn btn-primary">送出</button></a>
+				<br>
+				<br>
+				<br>
+</form>
+                </div>
+            </div>
+        </div>
         <!-- /#page-wrapper -->
-
-        <!--<a href=login.php>登出</a>
-        <a href=modifymember.php>修改</a>-->
-        <!--查詢-->
-	<div class=form-Search>
-		<form method="get" action="manage_member.php">
-
-			<table style="padding:5px;">
-				 <tr>
-					<td  colspan="10">資訊列表</td>
-				</tr>
-				<tr>
-					<td>帳號:</td>
-					<td><input type="text" name="key_id" id="key_id" value="<?=$_GET[key_id]?>"></td>
-				</tr>
-				<tr>
-					<td><input type="submit" value="查詢資料"></td>
-					<td><input type="reset" value="清除資料"></td>
-				</tr>
-			</table>
-		</form>
-總共有<?=$product_page_num?>人
-<div align="center">
-<ul class="pagination">
-<?for($i=1;$i<=$page;$i++){?>
-
- <li><a href=manage_member.php?p=<?=$i?>><?=$i?></a></li>
-<?}?>
-</ul>
-</div>
-    <!-- 表格表題-->
-	<div class='table-responsive'>
-    <table class='table table-striped'>
-    <tr>
-        <td data-th >編號</td>
-        <td data-th >帳號</td>
-        <td data-th >密碼</td>
-        <td data-th >姓名</td>
-        <td data-th >群組</td>
-        <td data-th >電話</td>
-        <td data-th >地址</td>
-        <td data-th >檔案</td>
-        <td data-th >時間</td>
-		<td data-th >刪除</td>
-        <td data-th >編輯</td>
-        <td data-th >刪除</td>
-    </tr>
-   <!--表格內容-->
-     <?while ($rmr=mysql_fetch_array($mr)) { ?> 
-    <tr>
-        <td data-th><?=$rmr['number']?></td>
-        <td data-th><?=$rmr['id']?></td>
-        <td data-th><?=$rmr['password']?></td>
-        <td data-th><?=$rmr['name']?></td>
-        <td data-th><?=$admin_group[$rmr['group_uid']]?></td>
-        <td data-th><?=$rmr['tel']?></td>
-        <td data-th><?=$rmr['address']?></td>
-        <td data-th><img src=./photo/personal/<?=$rmr['gif']?> width=50 height=50></td>
-        <td data-th><?=$rmr['memberdate']?></td>
-        <td data-th><?=$rmr['del']?></td>
-        <td data-th><a href=edit_manage_member.php?number=<?=$rmr['number']?>>編輯</a></td>
-        <td data-th><a href=del_manage_member.php?number=<?=$rmr['number']?>>刪除</a></td>
-    </tr>
-    <?}?>
-</table>
-</div></div>
-</div>
-            <!--內容S-->
-    <!-- /#wrapper -->
+        <!--內容S-->
+        <!-- /#wrapper -->
         <!-- jQuery -->
         <script src="bower_components/jquery/dist/jquery.min.js"></script>
 
@@ -427,7 +389,6 @@ $admin_group=array(1=>'訪客',2=>'會員',3=>'操作人員',4=>'管理者')	;
                 $('#dataTables-example').DataTable({
                     responsive: true
                 });
-
             });
         </script>
 
